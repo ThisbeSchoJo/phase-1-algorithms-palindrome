aa8e02c65f7b387cb484ea000deca92e56d46ba7
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-20 17:07:16 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = word.split("").reverse().join("")
+  if (word === reverseWord) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  if word is palindrome
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
-*/
+  - the split() method splits a string object into an array of substrings
+  - the reverse() method reverse the order of elements in an array
+  - the join() method joins all elements in an array into a string
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
